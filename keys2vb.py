2bca406d23cae75c9cad5dfcf4526591fd375077
wilas <limak.kamwil@gmail.com>
2014-06-11 20:13:15 +0200

    code cleaning

--- keys2vb.py
+++ keys2vb.py
@@ -35,42 +35,45 @@ def _make_scancodes(key_map, str_pattern):
     return scancodes
 
 def get_one_char_codes():
-    key_map = {'1234567890-=' : 0x02,
-        'qwertyuiop[]' : 0x10,
+    key_map = {
+        '1234567890-='  : 0x02,
+        'qwertyuiop[]'  : 0x10,
         'asdfghjkl;\'`' : 0x1e,
-        '\\zxcvbnm,./' : 0x2b
+        '\\zxcvbnm,./'  : 0x2b
     }
     scancodes = _make_scancodes(key_map, '%02x %02x')
     # Shift keys
-    key_map =  { '!@#$%^&*()_+' : 0x02,
-        'QWERTYUIOP{}' : 0x10,
-        'ASDFGHJKL:"~' : 0x1e,
-        '|ZXCVBNM<>?' : 0x2b
+    key_map =  {
+        '!@#$%^&*()_+'  : 0x02,
+        'QWERTYUIOP{}'  : 0x10,
+        'ASDFGHJKL:"~'  : 0x1e,
+        '|ZXCVBNM<>?'   : 0x2b
     }
     scancodes.update(_make_scancodes(key_map, '2a %02x %02x aa'))
     return scancodes
 
 def get_multi_char_codes():
-    scancodes = {}
-    scancodes['<Enter>'] = '1c 9c'
-    scancodes['<Backspace>'] = '0e 8e'
-    scancodes['<Spacebar>'] = '39 b9'
-    scancodes['<Return>'] = '1c 9c'
-    scancodes['<Esc>'] = '01 81'
-    scancodes['<Tab>'] = '0f 8f'
-    scancodes['<KillX>'] = '1d 38 0e b8'
-    scancodes['<Wait>'] = 'wait'
-    scancodes['<Up>'] = '48 c8'
-    scancodes['<Down>'] = '50 d0'
-    scancodes['<PageUp>'] = '49 c9'
-    scancodes['<PageDown>'] = '51 d1'
-    scancodes['<End>'] = '4f cf'
-    scancodes['<Insert>'] = '52 d2'
-    scancodes['<Delete>'] = '53 d3'
-    scancodes['<Left>'] = '4b cb'
-    scancodes['<Right>'] = '4d cd'
-    scancodes['<Home>'] = '47 c7'
-    scancodes['<Lt>'] = '2a 33 b3 aa', # to type '<' in some contexts - e.g. <Enter> literally
+    scancodes = {
+        '<Enter>'       : '1c 9c',
+        '<Backspace>'   : '0e 8e',
+        '<Spacebar>'    : '39 b9',
+        '<Return>'      : '1c 9c',
+        '<Esc>'         : '01 81',
+        '<Tab>'         : '0f 8f',
+        '<KillX>'       : '1d 38 0e b8',
+        '<Wait>'        : 'wait',
+        '<Up>'          : '48 c8',
+        '<Down>'        : '50 d0',
+        '<PageUp>'      : '49 c9',
+        '<PageDown>'    : '51 d1',
+        '<End>'         : '4f cf',
+        '<Insert>'      : '52 d2',
+        '<Delete>'      : '53 d3',
+        '<Left>'        : '4b cb',
+        '<Right>'       : '4d cd',
+        '<Home>'        : '47 c7',
+        '<Lt>'          : '2a 33 b3 aa', # to type '<' in some contexts - e.g. <Enter> literally
+    }
     # F1..F10
     for idx in range(1,10):
         scancodes['<F%s>' % idx] = '%02x' % (idx + 0x3a)
@@ -115,15 +118,15 @@ def translate_chars(input):
     # process single-char codes
     scancodes = get_one_char_codes()
     # convert input string to list
-    input = list(input)
+    input_list = list(input)
     # check only not assign yet (with value equal -1) cells in keys_array
     for index, _ in enumerate(keys_array):
         if keys_array[index] != -1:
             continue
         try:
-            keys_array[index] = scancodes[input[index]]
+            keys_array[index] = scancodes[input_list[index]]
         except KeyError:
-            sys.stderr.write('Error: Unknown symbol found - %s\n' % repr(input[index]))
+            sys.stderr.write('Error: Unknown symbol found - %s\n' % repr(input_list[index]))
             sys.exit(1)
 
     # remove empty string from keys_array
